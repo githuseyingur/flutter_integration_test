37d8a765266ed8d31e170d1aa0f81aa89f102e20
Hüseyin Gür <huseyingur.ceng@gmail.com>
2024-05-27 17:28:27 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:integrationtest/view/auth_view.dart';
 
 void main() {
@@ -10,6 +11,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: AuthView(),
